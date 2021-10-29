e4d81a8258a0afc1d775a03d79cd5fe70623c892
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-28 21:35:49 -0700

    added getAppointmentsByDay selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,26 @@
+export const getAppointmentsForDay = function(state, day) {
+  const resultArr = [];
+  
+  if (state.days.length === 0) {
+    return resultArr;
+  }
+
+  
+  const apptKeysArr = Object.keys(state.appointments);
+  
+  const dailyAppts = state.days.filter((elm) => {
+    return elm.name === day;
+  });
+
+  if (dailyAppts.length === 0) {
+    return resultArr;
+  }
+
+  for (const key of apptKeysArr) {
+    if (dailyAppts[0].appointments.includes(Number(key))) {
+      resultArr.push(state.appointments[key])
+    }
+  }
+
+  return resultArr;
+};
