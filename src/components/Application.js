305bc1a57d29558828234e102f481cb739d5bd16
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-28 22:54:05 -0700

    added complex state objects

--- src/components/Application.js
+++ src/components/Application.js
@@ -43,8 +43,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [selectedDay, setSelectedDay] = useState("Monday")
+  const [state, setState] = useState({
+    selectedDay: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setSelectedDay = selectedDay => setState({ ...state, selectedDay }); //creates a function called setSelectedDay which updates the state of the selectedDay state. CAN DO THIS FOR DAYS AND APPOINTMENTS TOO TO CREATE SEPEARTE ACTION STATE SETTERS FOR THEIR OWN SPECIFIC STATES.
+  const setDays = (days) => { //createing a specific setter above using different function notation
+    // setState({ ...state, days }) //this is removed cause of below notes
+    return setState(prev => ({...prev, days})); // this is needed because we are now referring to state in the effect method, but we haven't declared it in the dependency list. We don't want to make the request every time the component renders. Need to do below instead to remove the dependencey.
+  }
+
   const baseURL = "http://localhost:8001/api/days";
   const parsedAppointments = appointments.map((appts, index) => {
     return <Appointment key={appts.id} {...appts} />
@@ -53,10 +63,8 @@ export default function Application(props) {
   useEffect(() => {
     axios.get(baseURL)
     .then((response) => {
-      setDays((prev) => {
-        // console.log(response.data)
-        return prev = response.data;
-      })
+      // console.log(response.data);
+      return setDays(response.data);
     });
   }, []);
 
@@ -74,8 +82,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={selectedDay}
+          days={state.days}
+          value={state.selectedDay}
           onChange={setSelectedDay}
         />
       </nav>
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -3,6 +3,7 @@ import DayListItem from 'components/DayListItem';
 
 export default function DayList(props) {
   const { days, onChange, value  } = props; //Remember we expect a days array to be passed to our <DayList> component as a prop (and appropriately, we are passing an array of three days in our Storybook tests). Each object in the days array contains all the information to populate one <DayListItem> component.
+  // console.log(days)
   const parsedDaysArray = days.map((elm) => {
     if (value === elm.name) {
       return <DayListItem setDay={onChange} key={elm.id} spots={elm.spots} name={elm.name} selected />;
